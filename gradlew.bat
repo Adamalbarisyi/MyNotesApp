5b6f305ab889d3d1802aa7f669d55eca73641651
Adamalbarisyi <rifki7080@gmail.com>
2019-01-12 21:35:38 +0700

    first commit

---
+++ gradlew.bat
@@ -0,0 +1,44 @@
+<application>
+  <component name="ActionsCollector">
+    <counts>
+      <entry action="$Copy" count="48" />
+      <entry action="$Cut" count="14" />
+      <entry action="$Delete" count="108" />
+      <entry action="$Paste" count="53" />
+      <entry action="$Redo" count="622" />
+      <entry action="$SelectAll" count="118" />
+      <entry action="$Undo" count="4216" />
+      <entry action="Add TypeCast" count="1" />
+      <entry action="Add explicit type arguments" count="1" />
+      <entry action="AddToFavoritesPopup" count="1" />
+      <entry action="Android Lint Quick Fixes" count="3" />
+      <entry action="Android Quick Fixes" count="6" />
+      <entry action="Android.BuildApk" count="2" />
+      <entry action="Android.CreateResourceDirectory" count="19" />
+      <entry action="Android.CreateSampleDataDirectory" count="4" />
+      <entry action="Android.ExportProjectZip" count="9" />
+      <entry action="Android.RunLayoutInspector" count="1" />
+      <entry action="Android.SyncProject" count="30" />
+      <entry action="AndroidConnectDebuggerAction" count="1" />
+      <entry action="Back" count="3" />
+      <entry action="CleanGradleProject" count="53" />
+      <entry action="CloseProject" count="94" />
+      <entry action="CodeCleanup" count="2" />
+      <entry action="CodeCompletion" count="3" />
+      <entry action="CommentByBlockComment" count="1" />
+      <entry action="CommentByLineComment" count="28" />
+      <entry action="ConvertToWebp" count="1" />
+      <entry action="Create Class from New" count="1" />
+      <entry action="Create Class from Usage" count="6" />
+      <entry action="Create method from usage" count="1" />
+      <entry action="Debug" count="1" />
+      <entry action="DialogCancelAction Escape" count="1" />
+      <entry action="DialogOkAction Enter" count="23" />
+      <entry action="Editor notification option" count="1" />
+      <entry action="EditorBackSpace" count="17628" />
+      <entry action="EditorChooseLookupItem" count="1121" />
+      <entry action="EditorChooseLookupItemReplace" count="9093" />
+      <entry action="EditorCodeBlockStart" count="1" />
+      <entry action="EditorCopy" count="437" />
+      <entry action="EditorCut" count="130" />
+      <entry action="EditorDelete" count=
